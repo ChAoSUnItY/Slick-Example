d8fc956c1c79b02eb4eb51e6a936a5704daa6cd6
Kyle Lin <minecraft.kyle.train@gmail.com>
2026-07-29 16:10:50 +0800

    Add tag cloud

--- app/Main.hs
+++ app/Main.hs
@@ -1,96 +1,86 @@
-{-# LANGUAGE DeriveGeneric         #-}
-{-# LANGUAGE DeriveAnyClass        #-}
-{-# LANGUAGE OverloadedStrings     #-}
-{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
-import Control.Lens ((?~), at)
-import Control.Monad (void)
-import Data.Aeson (FromJSON, ToJSON, Value (Object, String), toJSON)
-import Data.Aeson.KeyMap (union)
-import Data.Aeson.Lens (_Object)
-import qualified Data.Text as T
-import Data.Time
-  ( UTCTime, defaultTimeLocale, formatTime, getCurrentTime
-  , iso8601DateFormat, parseTimeOrError)
-import Development.Shake
-  ( Action, Verbosity(Verbose), copyFileChanged, forP, getDirectoryFiles
-  , liftIO, readFile', shakeLintInside, shakeOptions, shakeVerbosity
-  , writeFile')
-import Development.Shake.Classes (Binary)
-import Development.Shake.FilePath ((</>), (-<.>), dropDirectory1)
-import Development.Shake.Forward (cacheAction, shakeArgsForward)
-import GHC.Generics (Generic)
-import Slick (compileTemplate', convert, markdownToHTML, substitute)
+import           Control.Lens
+import           Control.Monad
+import           Data.Aeson                 as A
+import           Data.Aeson.Lens
+import qualified Data.Map.Strict            as Map
+import           Development.Shake
+import           Development.Shake.Classes (Binary)
+import           Development.Shake.Forward
+import           Development.Shake.FilePath
+import           GHC.Generics               (Generic)
+import           Slick
 
-
----Config-----------------------------------------------------------------------
-
-siteMeta :: SiteMeta
-siteMeta =
-    SiteMeta { siteAuthor = "Kyle Lin"
-             , baseUrl = "https://ChAoSUnItY.github.io/Slick-Example/"
-             , siteTitle = "Example Slick Site"
-             , twitterHandle = Nothing
-             , githubUser = Just "ChAoSUnItY"
-             }
+import qualified Data.Text                  as T
 
 outputFolder :: FilePath
 outputFolder = "docs/"
 
---Data models-------------------------------------------------------------------
-
-withSiteMeta :: Value -> Value
-withSiteMeta (Object obj) = Object $ union obj siteMetaObj
-  where
-    Object siteMetaObj = toJSON siteMeta
-withSiteMeta _ = error "only add site meta to objects"
-
-data SiteMeta =
-    SiteMeta { siteAuthor    :: String
-             , baseUrl       :: String -- e.g. https://example.ca
-             , siteTitle     :: String
-             , twitterHandle :: Maybe String -- Without @
-             , githubUser    :: Maybe String
-             }
-    deriving (Generic, Eq, Ord, Show, ToJSON)
+-- | A single entry in the tag cloud. `weight` is a 1-5 bucket, sized
+-- relative to the most-used tag, so the template can pick a font size
+-- purely from data (via a `tag-size-N` CSS class) without doing any
+-- math itself.
+data TagCount =
+  TagCount
+    { tag    :: String
+    , count  :: Int
+    , weight :: Int
+    } deriving (Generic, Show, FromJSON, ToJSON)
 
 -- | Data for the index page
 data IndexInfo =
   IndexInfo
-    { posts :: [Post]
+    { posts    :: [Post]
+    , tagCloud :: [TagCount]
     } deriving (Generic, Show, FromJSON, ToJSON)
 
-type Tag = String
-
--- | Data for a blog post
+-- | Data for a blog post.
+-- `tags` is optional (`Maybe [String]`) so posts written before you
+-- adopted tags -- or that simply don't have any -- still parse fine;
+-- aeson's generic decoder treats a missing/omitted "tags" key in the
+-- frontmatter as `Nothing`, same as it already does for `image`.
 data Post =
-    Post { title       :: String
-         , author      :: String
-         , content     :: String
-         , url         :: String
-         , date        :: String
-         , tags        :: [Tag]
-         , description :: String
-         , image       :: Maybe String
+    Post { title   :: String
+         , author  :: String
+         , content :: String
+         , url     :: String
+         , date    :: String
+         , image   :: Maybe String
+         , tags    :: Maybe [String]
          }
     deriving (Generic, Eq, Ord, Show, FromJSON, ToJSON, Binary)
 
-data AtomData =
-  AtomData { title        :: String
-           , domain       :: String
-           , author       :: String
-           , posts        :: [Post]
-           , currentTime  :: String
-           , atomUrl      :: String } deriving (Generic, ToJSON, Eq, Ord, Show)
+-- | A post's tags, defaulting to none.
+postTags :: Post -> [String]
+postTags = maybe [] id . tags
+
+-- | Build a tag cloud from every post's tags: alphabetical and deduped,
+-- with a 1-5 "weight" bucket sized relative to the most-used tag.
+buildTagCloud :: [Post] -> [TagCount]
+buildTagCloud ps =
+  let counts   = Map.toAscList . Map.fromListWith (+) $
+                   [ (t, 1 :: Int) | p <- ps, t <- postTags p ]
+      allCounts = map snd counts
+      maxCount  = maximum (1 : allCounts)
+      minCount  = minimum (maxCount : allCounts)
+      bucket n
+        | maxCount == minCount = 3
+        | otherwise =
+            1 + round (4 * fromIntegral (n - minCount)
+                         / fromIntegral (maxCount - minCount) :: Double)
+  in [ TagCount t n (bucket n) | (t, n) <- counts ]
 
 -- | given a list of posts this will build a table of contents
 buildIndex :: [Post] -> Action ()
 buildIndex posts' = do
   indexT <- compileTemplate' "site/templates/index.html"
-  let indexInfo = IndexInfo {posts = posts'}
-      indexHTML = T.unpack $ substitute indexT (withSiteMeta $ toJSON indexInfo)
+  let indexInfo = IndexInfo { posts = posts', tagCloud = buildTagCloud posts' }
+      indexHTML = T.unpack $ substitute indexT (toJSON indexInfo)
   writeFile' (outputFolder </> "index.html") indexHTML
 
 -- | Find and build all posts
@@ -110,9 +100,10 @@ buildPost srcPath = cacheAction ("build" :: T.Text, srcPath) $ do
   let postUrl = T.pack . dropDirectory1 $ srcPath -<.> "html"
       withPostUrl = _Object . at "url" ?~ String postUrl
   -- Add additional metadata we've been able to compute
-  let fullPostData = withSiteMeta . withPostUrl $ postData
+  let fullPostData = withPostUrl $ postData
   template <- compileTemplate' "site/templates/post.html"
   writeFile' (outputFolder </> T.unpack postUrl) . T.unpack $ substitute template fullPostData
+  -- Convert the metadata into a Post object
   convert fullPostData
 
 -- | Copy all static files from the listed folders to their destination
@@ -122,46 +113,13 @@ copyStaticFiles = do
     void $ forP filepaths $ \filepath ->
         copyFileChanged ("site" </> filepath) (outputFolder </> filepath)
 
-formatDate :: String -> String
-formatDate humanDate = toIsoDate parsedTime
-  where
-    parsedTime =
-      parseTimeOrError True defaultTimeLocale "%b %e, %Y" humanDate :: UTCTime
-
-rfc3339 :: Maybe String
-rfc3339 = Just "%H:%M:%SZ"
-
-toIsoDate :: UTCTime -> String
-toIsoDate = formatTime defaultTimeLocale (iso8601DateFormat rfc3339)
-
-buildFeed :: [Post] -> Action ()
-buildFeed posts' = do
-  now <- liftIO getCurrentTime
-  let atomData =
-        AtomData
-          { title = siteTitle siteMeta
-          , domain = baseUrl siteMeta
-          , author = siteAuthor siteMeta
-          , posts = mkAtomPost <$> posts'
-          , currentTime = toIsoDate now
-          , atomUrl = "/atom.xml"
-          }
-  atomTempl <- compileTemplate' "site/templates/atom.xml"
-  writeFile' (outputFolder </> "atom.xml") . T.unpack $ substitute atomTempl (toJSON atomData)
-    where
-      mkAtomPost :: Post -> Post
-      mkAtomPost p = p { date = formatDate $ date p }
-
 -- | Specific build rules for the Shake system
 --   defines workflow to build the website
 buildRules :: Action ()
 buildRules = do
   allPosts <- buildPosts
   buildIndex allPosts
-  buildFeed allPosts
   copyStaticFiles
 
 main :: IO ()
-main = do
-  let shOpts = shakeOptions { shakeVerbosity = Verbose, shakeLintInside = ["\\"]}
-  shakeArgsForward shOpts buildRules
+main = slick buildRules
